b74559aaf12f8481138b7749df4dc3d1b5eea645
Johnson Liu <johnson@awaworks.net>
2021-06-27 09:01:48 +0800

    Update Uninstall Script

--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,20 @@
 
 # v0.3.0
 
+# delete program file
 rm /usr/share/bin/server-autoconfig
 rm -r /opt/server-autoconfig
-rm /etc/server-autoconfig.yml
+
+# delete config folder
+rm -r /etc/server-autoconfig
+
 # delete data
 rm -r /var/cache/server-autoconfig
+
+# delete service files
+systemctl stop server-autoconfig.timer
+rm /usr/lib/systemd/system/server-autoconfig.service
+rm /usr/lib/systemd/system/server-autoconfig.timer
+rm /usr/lib/systemd/system/server-autoconfig@.service
+rm /usr/lib/systemd/system/server-autoconfig@.timer
+systemctl daemon-reload
