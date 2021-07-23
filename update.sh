3f3feaccaddd9153a0531ee0986c25c05d554dce
Johnson Liu <johnson@awaworks.net>
2021-07-24 01:35:41 +0800

    Fixed errors in update.sh

--- update.sh
+++ update.sh
@@ -10,9 +10,6 @@ git pull
 cp ./server-autoconfig.py /opt/server-autoconfig/
 chmod +x /opt/server-autoconfig/server-autoconfig.py
 
-# copy config file
-cp ./config.yml /etc/server-autoconfig/
-
 # copy service files
 cp ./systemd/* /usr/lib/systemd/system/
 systemctl daemon-reload
